af5f38991071936f381cb5f37b68c09fb0c6fd95
Mason Hill <mkhill86@gmail.com>
2025-05-04 09:05:27 -0400

    Practicing Javascript

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
-console.log('hello world')
+console.log('hello world');
+
+let personalInfo = {
+  name: "Full Name",
+  weight: "Weight",
+  height: "Height",
+};
+
+let storePerInfo = document.getElementsByClassName(personalInfo);
+
+storePerInfo.innerHTML = personalInfo.name;
+
+storePerInfo;
